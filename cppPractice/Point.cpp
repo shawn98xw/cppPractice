38b3fc4136494f476bf5f7793adf09a1d375fbb5
shawn98xw <shawn98xw@gmail.com>
2022-10-21 17:31:29 +0800

    -

--- cppPractice/Point.cpp
+++ cppPractice/Point.cpp
@@ -2,31 +2,31 @@
 using namespace std;
 
 Point::Point(double x, double y) : x(x), y(y) {
-    cout << "Point(double x = " << x << ", double y = " << y << ")" << endl;
-}
+        cout << "Point(double x = " << x << ", double y = " << y << ")" << endl;
+    }
 
-// 拷贝构造函数
+    // 拷贝构造函数
 Point::Point(const Point& p) {
-    // 打印点的值
-    cout << "Point(const Point &p:(" << p.x << ", " << p.y << ")" << endl;
-    this->x = p.x;
-    this->y = p.y;
-}
+        // 打印点的值
+        cout << "Point(const Point &p:(" << p.x << ", " << p.y << ")" << endl;
+        this->x = p.x;
+        this->y = p.y;
+    }
 
-// 析构函数，由于没有申请内存，析构函数中不需要做什么
+    // 析构函数，由于没有申请内存，析构函数中不需要做什么
 Point::~Point() {
-    cout << "~Point():(" << x << ", " << y << ")" << endl;
-}
+        cout << "~Point():(" << x << ", " << y << ")" << endl;
+    }
 
-// x, y 绑定的成成员函数
+    // x, y 绑定的成成员函数
 void Point::setPoint(const Point& p) {
-    this->x = p.x;
-    this->y = p.y;
-}
+        this->x = p.x;
+        this->y = p.y;
+    }
 void Point::setPoint(double x, double y) {
-    this->x = x;
-    this->y = y;
-}
+        this->x = x;
+        this->y = y;
+    }
 void Point::setX(double x) { this->x = x; }
 void Point::setY(double y) { this->y = y; }
 double Point::getX() { return x; }
